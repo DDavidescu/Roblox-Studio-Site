43af1a71fd049e102140494c16e40b75c4f4b2a7
DDavidescu <dbau43@yahoo.com>
2025-11-29 18:50:29 +0200

    Build docs for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: isProd ? "/RobloxGameStudio/" : "/",
+  build: {
+    outDir: "docs",
+  },
+});
