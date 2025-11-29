88a9848a798c2942430371f129adc09c48ed7c05
DDavidescu <dbau43@yahoo.com>
2025-11-29 20:22:31 +0200

    Initial upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const isProd = process.env.NODE_ENV === "production";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isProd ? "/RobloxGameStudio/" : "/",
-  build: {
-    outDir: "docs",
-  },
-});
+})
